c4b23d37f01fe1ad3a7419d6711667431b2c79fe
thecoderguy11 <linuxworms@gmail.com>
2020-01-23 18:55:31 +0530

    created user model

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "users/show", type: :view do
   before(:each) do
-    @user = assign(:user, User.create!())
+    @user = assign(:user, User.create!(
+      :fname => "Fname",
+      :lname => "Lname",
+      :email => "Email"
+    ))
   end
 
   it "renders attributes in <p>" do
     render
+    expect(rendered).to match(/Fname/)
+    expect(rendered).to match(/Lname/)
+    expect(rendered).to match(/Email/)
   end
 end
